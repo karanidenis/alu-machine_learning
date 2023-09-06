91959a6cb674e4c49494a21b8d20f53ecdcf8fbc
karanidenis <d.waweru@alustudent.com>
2023-09-06 16:41:27 +0300

    adding matrices without using numpy

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 def add_arrays(arr1, arr2):
-    # function adds two arrays element-wise
+    """ function adds two arrays element-wise """
     new_matrix = []
-    
+
     if isinstance(arr1, list) and isinstance(arr2, list):
         if len(arr1) == len(arr2):
             for i in range(len(arr1)):
