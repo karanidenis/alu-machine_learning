37f41077cb69dd85ffe38947ae8098064bb107d1
karanidenis <d.waweru@alustudent.com>
2023-09-06 16:59:10 +0300

    adding matrices element-wise

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -7,3 +7,13 @@ This module contains the function adds two matrices element-wise
 def add_matrices2D(mat1, mat2):
     """ function adds two matrices element-wise """
     
+    new_matrix = []
+    
+    # check if matrix has more than list
+    if len(mat1) == len(mat2):
+        for arr1, arr2 in zip(mat1, mat2):
+            if len(arr1) == len(arr2):
+                new_matrix.append([x + y for x, y in zip(arr1, arr2)])
+            else:
+                return None
+        return new_matrix
