53ebb76e692f9da3324e1dd0e92dab520971a844
karanidenis <d.waweru@alustudent.com>
2023-09-06 17:18:01 +0300

    concatenate two arrays

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -4,9 +4,10 @@
 This module contains the function that can concatenate two arrays
 """
 
+
 def cat_arrays(arr1, arr2):
     """
     function that can concatenate two arrays
     """
-    
+
     return arr1 + arr2
---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This module contains the function that can concatenate 
+two matrices along a specific axis
+"""
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    fuction that can concatenate two matrices along a specific axis
+    """
+    
+    
