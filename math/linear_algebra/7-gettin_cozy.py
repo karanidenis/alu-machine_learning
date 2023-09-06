69d374d2b5fa939d539d1e30a05280a800ad50e4
karanidenis <d.waweru@alustudent.com>
2023-09-06 17:24:58 +0300

    concatenate two 2Dmatrices

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,9 +5,18 @@ This module contains the function that can concatenate
 two matrices along a specific axis
 """
 
+
 def cat_matrices2D(mat1, mat2, axis=0):
     """
     fuction that can concatenate two matrices along a specific axis
+    matrices are 2D represented as lists of lists
     """
-    
-    
+
+    if axis == 0:
+        new_matrix = mat1 + mat2
+    elif axis == 1:
+        new_matrix = [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+    else:
+        raise ValueError("Axis must be 0 or 1 for concatenation.")
+
+    return new_matrix
