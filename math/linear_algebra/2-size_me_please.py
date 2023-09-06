4017ec3a13a521f79a62ea253c998e6b5671886e
karanidenis <d.waweru@alustudent.com>
2023-09-06 15:54:17 +0300

    calculating shape without using numpy

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+# def matrix_shape(matrix):
+#     import numpy as np
+#     return np.array(matrix).shape
+
 def matrix_shape(matrix):
-    import numpy as np
-    return np.array(matrix).shape
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] if matrix else None
+    return shape
---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def matrix_transpose(matrix):
+    import numpy as np
+    return np.transpose(matrix)
