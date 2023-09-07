dec28fc030b18547d9dd68894d120f88e6022225
karanidenis <d.waweru@alustudent.com>
2023-09-07 12:05:33 +0300

    add matrices while checking the shape

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -13,11 +13,24 @@ def add_matrices(mat1, mat2):
     return None when shape is not same
     """
 
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
+    # mat1 = np.array(mat1)
+    # mat2 = np.array(mat2)
 
-    if mat1.shape != mat2.shape:
+    # if mat1.shape != mat2.shape:
+    #     return None
+
+    # result = mat1 + mat2
+    # return result.tolist()
+
+    if isinstance(mat1, int) and isinstance(mat2, int):
+        return mat1 + mat2
+    elif isinstance(mat1, list) and isinstance(mat2, list):
+        if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
+            return None
+
+        result = []
+        for row1, row2 in zip(mat1, mat2):
+            result.append(add_matrices(row1, row2))
+        return result
+    else:
         return None
-
-    result = mat1 + mat2
-    return result.tolist()
