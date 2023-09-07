41332b3642e75daca01242bf9a43a92d8c396443
karanidenis <d.waweru@alustudent.com>
2023-09-07 10:52:52 +0300

    multiply matrices with different shapes using .dot

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -6,3 +6,11 @@ assume mat1 and mat2 are never empty and
 can be interpreted as numpy.ndarray
 """
 
+
+def np_matmul(mat1, mat2):
+    """"
+    matrix multiplication
+    no loops ot conditions
+    """
+    mult = np.dot(mat1, mat2)
+    return mult
