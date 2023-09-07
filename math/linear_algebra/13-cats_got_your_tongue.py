28076c42043c6807684f53f87f26a3bec47f7209
karanidenis <d.waweru@alustudent.com>
2023-09-07 11:00:34 +0300

    concatenate matrices on a specific asix

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -5,13 +5,13 @@ concatenates two matrices along an axis
 assume mat1 and mat2 are never empty and
 can be interpreted as numpy.ndarray
 """
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """
     function to concatenate two matrices along an axis
     """
-    import numpy as np
 
     axis_mat = np.concatenate((mat1, mat2), axis=axis)
     return axis_mat
