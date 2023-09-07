729562818a1adbd9d87ac73639352be03947337c
karanidenis <d.waweru@alustudent.com>
2023-09-07 09:18:03 +0300

    shape of a numpy.ndarray

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+This module contains the function:
+calculates the shape of a numpy.ndarray
+do not use try/except
+"""
+
+
+def np_shape(matrix):
+    """
+    function calculates shape of numpy.ndarray
+    """
+
+    new_matrix = np.array(matrix)
+
+    return new_matrix.shape
