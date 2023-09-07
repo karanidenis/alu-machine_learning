9eb7ed667cd2bdacf299775f9d0d4045e92032e5
karanidenis <d.waweru@alustudent.com>
2023-09-07 09:30:30 +0300

    transpose matrices

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-import numpy as np
+# import numpy as np
 """
 This module contains the function:
-c
+transposes matrix
 """
 
 
 def np_transpose(matrix):
+    """
+    assume matrix is translated as numpy.ndarray
+    """
+    if len(matrix) > 0:
+        new_matrix = matrix.transpose
+        return new_matrix
+    else:
+        return []
