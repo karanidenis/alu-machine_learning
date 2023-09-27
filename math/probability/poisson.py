c5f442cc8d3e009e85e5a01f1cdcfbe2dcc13287
karanidenis <d.waweru@alustudent.com>
2023-09-27 15:01:51 +0300

    Probability for machine learning - Poisson distribution

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -27,3 +27,30 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of “successes”
+        """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial = factorial * i
+        return ((self.lambtha ** k) * (2.7182818285 ** (-self.lambtha))) / factorial
+    
+    def cdf(self, k):
+        """
+        Calculate the value of the CDF for a given number of “successes”
+        k = "successes"
+        """
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        result = 0
+        for i in range(k + 1):
+            result = result + self.pmf(i)
+        return result
