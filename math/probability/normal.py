c982f7a12e5b50355787a2092f3a1596dfad67fb
karanidenis <d.waweru@alustudent.com>
2023-09-27 18:12:28 +0300

    Probability for machine learning - Normal distribution

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -19,13 +19,29 @@ class Normal:
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
-            self.stddev = stddev
-            self.mean = mean
+            self.stddev = float(stddev)
+            self.mean = float(mean)
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             else:
-                self.mean = sum(data) / len(data)
-                self.stddev = (sum([(x - self.mean) ** 2 for x in data]))
+                self.mean = float(sum(data) / len(data))
+                self.stddev = float(sum([(x - self.mean) ** 2 for x in data]))
+
+    def z_score(self, x):
+        """
+        calculates z-score of a given x-value
+        x is the value
+        """
+        self.x = float((self.x - self.mean) / self.stddev)
+        return self.x
+
+    def x_value(self, z):
+        """
+        calculates x-value of the given z-score
+        z - z-score
+        """
+        x_value = (self.z * self.stddev) + self.mean
+        return x_value
