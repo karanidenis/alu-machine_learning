031fe8bb089ea4a5852c4847ece0f62d17974511
karanidenis <d.waweru@alustudent.com>
2023-09-27 18:38:44 +0300

    Probability for machine learning - Normal distribution

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -29,7 +29,8 @@ class Normal:
             else:
                 self.mean = float(sum(data) / len(data))
                 squared_diff = [(x - self.mean) ** 2 for x in data]
-                self.stddev = float((sum(squared_diff) / (len(data) - 1)) ** 0.5)
+                self.stddev = float(
+                    (sum(squared_diff) / (len(data) - 1)) ** 0.5)
 
     def z_score(self, x):
         """
@@ -48,3 +49,21 @@ class Normal:
         return (z * self.stddev) + self.mean
         # x_value = (z * self.stddev) + self.mean
         # return x_value
+
+    def pdf(self, x):
+        """
+        calculates the value of the PDF for a given x-value
+        x - the x-value
+        """
+        import math
+        pi = 3.1415926536
+        e = 2.7182818285
+        exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
+        pdf = (1 / (self.stddev * math.sqrt(2 * pi))) * e **(exponent)
+        return pdf
+        # import math
+        # pi = 3.1415926536
+        # e = 2.7182818285
+        # pdf = (1 / (self.stddev * math.sqrt(2 * pi))) * e ** (
+        #     -((x - self.mean) ** 2) / (2 * self.stddev ** 2))
+        # return pdf
