db1d6fe877748c0f12dc7ace99fc87375840e82d
karanidenis <d.waweru@alustudent.com>
2023-09-27 17:19:15 +0300

    Probability for machine learning - Poisson distribution

---
+++ math/probability/exponential.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This class represents a exponential distribution
+"""
+
+class Exponential:
+    """
+    This class represents a exponential distribution
+    """
+    
+    def __init__(self, data=None, lambtha=1.):
