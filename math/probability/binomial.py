ce8de9b3d70a94c5317d59a9f6340c4c9f9fa1cf
karanidenis <d.waweru@alustudent.com>
2023-10-01 22:01:20 +0300

    binormial distribution

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -18,23 +18,22 @@ class Binomial:
         """
         if data is None:
             if n <= 0:
-                raise ValueError("n must be a positive value")
-            if not (0 < p < 1):
-                raise ValueError("p must be greater than 0 and less than 1")
-            else:
-                self.n = int(n)
-                self.p = float(p)
+                raise ValueError('n must be a positive value')
+            if p <= 0 or p >= 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.n = int(n)
+            self.p = float(p)
         else:
             if not isinstance(data, list):
-                raise TypeError("data must be a list")
+                raise TypeError('data must be a list')
             if len(data) < 2:
-                raise ValueError("data must contain multiple values")
+                raise ValueError('data must contain multiple values')
+            mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+            p = 1.0 - variance / mean
+            self.n = round((mean / p))
+            self.p = float(mean / self.n)
 
-            self.p = sum(data) / len(data)
-            self.n = round(len(data) * self.p)
-            self.p = self.n / len(data)
-            self.n = int(self.n)
-        
     def pmf(self, k):
         """
         Calculates the value of the PMF for a given number of "successes" k without using external libraries.
