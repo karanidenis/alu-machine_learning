70c4e3979f87557bec15349a7864d93e0ba89938
karanidenis <d.waweru@alustudent.com>
2023-10-01 21:44:37 +0300

    normal distribution

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -16,8 +16,9 @@ class Normal:
         mean - mean of the distribution
         stddev - standard deviation
         """
-        self.E = 2.7182818285
-        self.PI = 3.1415926536
+        # Updated values for pi and Euler's number
+        self.PI = 3.141592653589793
+        self.E = 2.718281828459045
 
         if data is None:
             if stddev <= 0:
@@ -35,7 +36,7 @@ class Normal:
             self.mean = float(sum(data) / len(data))
 
             squared_diff = [(x - self.mean) ** 2 for x in data]
-            self.stddev = (sum(squared_diff) / (len(data) - 1)) ** 0.5
+            self.stddev = (sum(squared_diff) / len(data)) ** 0.5  # Use the population standard deviation formula
 
     def z_score(self, x):
         """
@@ -64,8 +65,13 @@ class Normal:
         # exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
         # pdf = (1 / (self.stddev * (2 * pi) ** 0.5)) * e ** (exponent)
         # return pdf
-        return (self.E ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
-                / (self.stddev * ((2 * self.PI) ** 0.5)))
+        # factor = float(1 / ((self.stddev) * (2 * self.PI) ** 0.5))
+        # exp = -0.5 * (((x - self.mean) / self.stddev) ** 2)
+        # return float(factor * (self.E ** exp))
+        exponent = -((x - self.mean) ** 2) / (2 * (self.stddev ** 2))
+        pdf_value = (1 / (self.stddev * ((2 * self.PI) ** 0.5))) * (self.E ** exponent)
+        return pdf_value
+
 
     def cdf(self, x):
         """
