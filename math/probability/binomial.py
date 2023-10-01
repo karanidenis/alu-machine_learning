adfa2170a51e623594e30fb369b6f39af5631cce
karanidenis <d.waweru@alustudent.com>
2023-10-01 20:58:01 +0300

    fix cdf

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -34,3 +34,35 @@ class Binomial:
             self.n = round(len(data) * self.p)
             self.p = self.n / len(data)
             self.n = int(self.n)
+        
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of "successes" k without using external libraries.
+        k - The number of "successes" for which you want to calculate the PMF.
+        Returns the PMF value for k.
+        """
+        k = int(k)  # Convert k to an integer
+        if k < 0 or k > self.n:
+            return 0
+        else:
+            # Calculate the binomial coefficient (n choose k) using dynamic programming
+            def calculate_binomial_coefficient(n, k):
+                dp = [[0] * (k + 1) for _ in range(n + 1)]
+                for i in range(n + 1):
+                    dp[i][0] = 1
+                for i in range(1, n + 1):
+                    for j in range(1, min(i, k) + 1):
+                        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                return dp[n][k]
+
+            binomial_coeff = calculate_binomial_coefficient(self.n, k)
+
+            # Calculate (self.p ** k) and ((1 - self.p) ** (self.n - k))
+            p_power_k = self.p
+            q_power_n_minus_k = 1 - self.p
+            for i in range(1, k):
+                p_power_k *= self.p
+                q_power_n_minus_k *= (1 - self.p)
+
+            pmf = binomial_coeff * p_power_k * q_power_n_minus_k
+            return pmf
--- math/probability/normal.py
+++ math/probability/normal.py
@@ -16,11 +16,16 @@ class Normal:
         mean - mean of the distribution
         stddev - standard deviation
         """
+        self.E = 2.7182818285
+        self.PI = 3.1415926536
+        
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
+            else:
+                self.stddev = float(stddev)
             self.mean = float(mean)
-            self.stddev = float(stddev)
+            
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
@@ -46,40 +51,34 @@ class Normal:
         calculates x-value of the given z-score
         z - z-score
         """
-        return (z * self.stddev) + self.mean
+        # return (z * self.stddev) + self.mean
         # x_value = (z * self.stddev) + self.mean
         # return x_value
+        return self.mean + z * self.stddev
 
     def pdf(self, x):
         """
         calculates the value of the PDF for a given x-value
         x - the x-value
-        """
-        pi = 3.1415926536
-        e = 2.7182818285
-        exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
-        pdf = (1 / (self.stddev * (2 * pi) ** 0.5)) * e ** (exponent)
-        return pdf
-
-        # pi = 3.1415926536
-        # e = 2.7182818285
-        # pdf = (1 / (self.stddev * math.sqrt(2 * pi))) * e ** (
-        #     -((x - self.mean) ** 2) / (2 * self.stddev ** 2))
+        # """
+        # exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
+        # pdf = (1 / (self.stddev * (2 * pi) ** 0.5)) * e ** (exponent)
         # return pdf
+        return (self.E ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
+                / (self.stddev * ((2 * self.PI) ** 0.5)))
 
     def cdf(self, x):
         """
         calculates the value of the CDF for a given x-value
         x - the x-value
         """
-        pi = 3.1415926536
-        e = 2.7182818285
-        exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
-        erf = (2 / (pi ** 0.5)) * (x - ((x ** 3) / 3) + ((x ** 5) / 10) -
-                                   ((x ** 7) / 42) + ((x ** 9) / 216))
-        print(erf)
-        cdf = 0.5 * (1 + erf * (e ** exponent))
-        return cdf
+        # exponent = -((x - self.mean) ** 2) / (2 * self.stddev ** 2)
+        # erf = (2 / (pi ** 0.5)) * (x - ((x ** 3) / 3) + ((x ** 5) / 10) -
+        #                            ((x ** 7) / 42) + ((x ** 9) / 216))
+        # print(erf)
+        # cdf = 0.5 * (1 + erf * (e ** exponent))
+        return (0.5 * (1 + self.erf((x - self.mean) /
+                                    (self.stddev * 2 ** 0.5))))
 
         # pi = 3.1415926536
         # e = 2.7182818285
