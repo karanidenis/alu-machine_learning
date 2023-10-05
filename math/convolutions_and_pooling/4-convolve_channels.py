5bc70a90ebd7d11e4d60b39b746345922ef6dd6b
karanidenis <d.waweru@alustudent.com>
2023-10-05 11:03:37 +0300

    convolution with channels

---
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+This module has the method that performs
+convolution on grayscale images with channels
+"""
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """
+    convolution with channels
+    """
+    m, h, w, c = images.shape
+    kh, kw, kc = kernel.shape
+    # stride
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = int(((h - 1) * sh + kh - h) / 2)
+        pw = int(((w - 1) * sw + kw - w) / 2)
+    elif isinstance(padding, tuple):
+        ph, pw = padding
+    else:
+        ph, pw = 0, 0
+
+    # output
+    out_h = int((h - kh + 2 * ph) / sh) + 1
+    out_w = int((w - kw + 2 * pw) / sw) + 1
+
+    # initialise convolved images
+    convolved_images = np.zeros((m, out_h, out_w))
+
+    for i in range(out_h):
+        for j in range(out_w):
+            for k in range(m):
+                # Extract a patch from the image
+                patch = images[k, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
+                for ch in range(c):
+                    convolved_images[k, i,
+                                     j] += np.sum(patch[:, :, ch] * kernel[:, :, ch])
+
+    return convolved_images
