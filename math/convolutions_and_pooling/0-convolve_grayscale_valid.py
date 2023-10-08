e12bf361707e10421f8b7a0c6065cc6c70c33e51
karanidenis <d.waweru@alustudent.com>
2023-10-08 17:05:06 +0300

    valid convolution

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -22,31 +22,21 @@ def convolve_grayscale_valid(images, kernel):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    # # Calculate the output size for "valid" convolution
-    # out_h = h - kh + 1
-    # out_w = w - kw + 1
+    # Calculate the output size for "valid" convolution
+    out_h = h - kh + 1
+    out_w = w - kw + 1
 
-    # # Initialize the output array
-    # convolved_images = np.zeros((m, out_h, out_w))
+    # Initialize the output array
+    convolved_images = np.zeros((m, out_h, out_w))
 
-    # # Perform "valid" convolution with two for loops
-    # for i in range(out_h):
-    #     for j in range(out_w):
-    #         # Extract a patch from the image
-    #         patch = images[:, i:i + kh, j:j + kw]
-    #         # Perform element-wise multiplication and sum
-    #         convolved_images[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
-    #         print(convolved_images)
+    # Perform "valid" convolution with two for loops
+    for i in range(out_h):
+        for j in range(out_w):
+            # Extract a patch from the image
+            patch = images[:, i:i + kh, j:j + kw]
+            # Perform element-wise multiplication and sum
+            convolved_images[:, i, j] = (kernel * images[:, i: i + kh, j: j + kw])\
+                .sum(axis=(1, 2))
+            print(convolved_images)
 
-    # return convolved_images
-
-    ph = int(kh / 2)
-    pw = int(kw / 2)
-    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
-    convoluted = np.zeros((m, h, w))
-    for h in range(h):
-        for w in range(w):
-            square = padded[:, h: h + kh, w: w + kw]
-            insert = np.sum(square * kernel, axis=1).sum(axis=1)
-            convoluted[:, h, w] = insert
-    return convoluted
+    return convolved_images
--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -23,20 +23,31 @@ def convolve_grayscale_same(images, kernel):
     kh, kw = kernel.shape
     m, h, w = images.shape
     kh, kw = kernel.shape
-    # Calculate the padding required to maintain the same output size
-    padding_h = kh // 2
-    padding_w = kw // 2
-    padded_images = np.pad(
-        images, ((0, 0), (padding_h, padding_h), (padding_w, padding_w)),
-        mode='constant')
+    # # Calculate the padding required to maintain the same output size
+    # padding_h = kh // 2
+    # padding_w = kw // 2
+    # padded_images = np.pad(
+    #     images, ((0, 0), (padding_h, padding_h), (padding_w, padding_w)),
+    #     mode='constant')
 
-    # Perform convolution
-    convolved_images = np.zeros((m, h, w))
-    for i in range(m):
-        for j in range(h):
-            for k in range(w):
-                patch = padded_images[i, j:j + kh, k:k + kw]
-                convolved_images[i, j, k] = np.sum(patch * kernel)
-                print(convolved_images)
+    # # Perform convolution
+    # convolved_images = np.zeros((m, h, w))
+    # for i in range(m):
+    #     for j in range(h):
+    #         for k in range(w):
+    #             patch = padded_images[i, j:j + kh, k:k + kw]
+    #             convolved_images[i, j, k] = np.sum(patch * kernel)
+    #             print(convolved_images)
 
-    return convolved_images
+    # return convolved_images
+
+    ph = int(kh / 2)
+    pw = int(kw / 2)
+    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+    convoluted = np.zeros((m, h, w))
+    for h in range(h):
+        for w in range(w):
+            square = padded[:, h: h + kh, w: w + kw]
+            insert = np.sum(square * kernel, axis=1).sum(axis=1)
+            convoluted[:, h, w] = insert
+    return convoluted
