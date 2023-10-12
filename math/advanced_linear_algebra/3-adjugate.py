9183db7d9763a22baae6f0828d48dca86de483ba
karanidenis <d.waweru@alustudent.com>
2023-10-12 17:18:44 +0300

    linear transformations

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -18,7 +18,7 @@ def adjugate(matrix):
     if len(matrix) == 1:
         return [[1]]
     if len(matrix) == 2:
-        return [[matrix[1][1], -matrix[1][0]], [matrix[0][1], -matrix[0][0]]]
+        return [[matrix[1][1], -matrix[0][1]], [-matrix[1][0], matrix[0][0]]]
     if len(matrix) == 3:
         a = (matrix[1][1]*matrix[2][2]) - (matrix[1][2]*matrix[2][1])
         b = (matrix[1][0]*matrix[2][2]) - (matrix[1][2]*matrix[2][0])
