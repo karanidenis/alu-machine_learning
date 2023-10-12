1f3b9ba7fd0d58f4c4026b18398f3955e8fa6fb3
karanidenis <d.waweru@alustudent.com>
2023-10-12 17:17:05 +0300

    linear transformations

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+
+"""
+this module has the function inverse(matrix)
+"""
+
+
+def inverse(matrix):
+    """
+    calculates the inverse
+    """
+    if len(matrix) < 1:
+        raise TypeError('matrix must be a list of lists')
+    for row in matrix:
+        if len(row) != len(matrix):
+            raise ValueError('matrix must be a non-empty square matrix')
+
+    if len(matrix) == 1:
+        return [[1]]
+    if len(matrix) == 2:
+        return [[matrix[1][1], -matrix[1][0]], [matrix[0][1], -matrix[0][0]]]
+    if len(matrix) == 3:
+        a = (matrix[1][1]*matrix[2][2]) - (matrix[1][2]*matrix[2][1])
+        b = (matrix[1][0]*matrix[2][2]) - (matrix[1][2]*matrix[2][0])
+        c = (matrix[1][0]*matrix[2][1]) - (matrix[1][1]*matrix[2][0])
+        d = (matrix[0][1]*matrix[2][2]) - (matrix[0][2]*matrix[2][1])
+        e = (matrix[0][0]*matrix[2][2]) - (matrix[0][2]*matrix[2][0])
+        f = (matrix[0][0]*matrix[2][1]) - (matrix[0][1]*matrix[2][0])
+        g = (matrix[0][1]*matrix[1][2]) - (matrix[0][2]*matrix[1][1])
+        h = (matrix[0][0]*matrix[1][2]) - (matrix[0][2]*matrix[1][0])
+        i = (matrix[0][0]*matrix[1][1]) - (matrix[0][1]*matrix[1][0])
+    # result = [[a, -b, c], [-d, e, -f], [g, -h, i]]
+    adjugated_matrix = [[a, -d, g], [-b, e, -h], [c, -f, i]]
+    return adjugated_matrix
