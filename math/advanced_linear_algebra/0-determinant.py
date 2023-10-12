c8316be739095f0f33923aa4033d32ec156a542b
karanidenis <d.waweru@alustudent.com>
2023-10-12 13:20:32 +0300

    linear transformations

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -12,8 +12,8 @@ def determinant(matrix):
     if matrix == [[]]:
         return 1
     if len(matrix) < 1:
-    # and not isinstance(matrix[0], list):
-            raise TypeError('matrix must be a list of lists')
+        # and not isinstance(matrix[0], list):
+        raise TypeError('matrix must be a list of lists')
     for row in matrix:
         if len(row) != len(matrix):
             raise ValueError('matrix must be a square matrix')
@@ -26,10 +26,10 @@ def determinant(matrix):
         val1 = matrix[0][0]*matrix[1][1]*matrix[2][2]
         val2 = matrix[0][1]*matrix[1][2]*matrix[2][0]
         val3 = matrix[0][2]*matrix[1][0]*matrix[2][1]
-        
-        val4 =  matrix[0][2]*matrix[1][1]*matrix[2][0]
+
+        val4 = matrix[0][2]*matrix[1][1]*matrix[2][0]
         val5 = matrix[0][1]*matrix[1][0]*matrix[2][2]
         val6 = matrix[0][0]*matrix[1][2]*matrix[2][1]
-        
+
         result = (val1 + val2 + val3) - (val4 + val5 + val6)
         return result
