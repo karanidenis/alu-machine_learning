3e05d435652ae8a13e730238e2d9af57b6ec8b29
karanidenis <d.waweru@alustudent.com>
2023-10-13 15:30:05 +0300

    linear transformations

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -14,15 +14,18 @@ def determinant(matrix):
     if len(matrix) < 1:
         # and not isinstance(matrix[0], list):
         raise TypeError('matrix must be a list of lists')
-    for row in matrix:
-        if len(row) != len(matrix):
-            raise ValueError('matrix must be a square matrix')
+    # for row in matrix:
+    #     if len(row) != len(matrix):
+    #         raise ValueError('matrix must be a square matrix')
+    if not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError('matrix must be a square matrix')
+
 
     if len(matrix) == 1:
         return matrix[0][0]
-    if len(matrix) == 2:
+    elif len(matrix) == 2:
         return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
-    if len(matrix) == 3:
+    elif len(matrix) == 3:
         val1 = matrix[0][0]*matrix[1][1]*matrix[2][2]
         val2 = matrix[0][1]*matrix[1][2]*matrix[2][0]
         val3 = matrix[0][2]*matrix[1][0]*matrix[2][1]
@@ -33,3 +36,16 @@ def determinant(matrix):
 
         result = (val1 + val2 + val3) - (val4 + val5 + val6)
         return result
+
+    else:
+        det = 0
+        for i in range(len(matrix)):
+            cofactor = matrix[0][i] * determinant(get_minor(matrix, 0, i))
+            if i % 2 == 0:  # Use the sign of the permutation to determine the sign of the cofactor
+                det += cofactor
+            else:
+                det -= cofactor
+        return det
+
+def get_minor(matrix, row, col):
+    return [[matrix[i][j] for j in range(len(matrix)) if j != col] for i in range(len(matrix)) if i != row]
