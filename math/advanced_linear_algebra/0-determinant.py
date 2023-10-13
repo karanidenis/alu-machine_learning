c50846ca12046a867a6365d63607f828fadbfd0d
karanidenis <d.waweru@alustudent.com>
2023-10-13 15:49:27 +0300

    linear transformations

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -14,7 +14,8 @@ def determinant(matrix):
     # if len(matrix) < 1:
     #     # and not isinstance(matrix[0], list):
     #     raise TypeError('matrix must be a list of lists')
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+                                               for row in matrix):
         raise TypeError('matrix must be a list of lists')
     # for row in matrix:
     #     if len(row) != len(matrix):
