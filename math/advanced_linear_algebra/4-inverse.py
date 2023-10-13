a47ec385caf2f08007331e0809ff7c3f8363801f
karanidenis <d.waweru@alustudent.com>
2023-10-13 16:01:27 +0300

    linear transformations

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -4,14 +4,19 @@
 this module has the function inverse(matrix)
 """
 determinant = __import__('0-determinant').determinant
+adjugate = __import__('3-adjugate').adjugate
 
 
 def inverse(matrix):
     """
     calculates the inverse
     """
-    if len(matrix) < 1:
+    # if len(matrix) < 1:
+    #     raise TypeError('matrix must be a list of lists')
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+                                               for row in matrix):
         raise TypeError('matrix must be a list of lists')
+
     for row in matrix:
         if len(row) != len(matrix):
             raise ValueError('matrix must be a non-empty square matrix')
@@ -39,10 +44,18 @@ def inverse(matrix):
         g = (matrix[0][1]*matrix[1][2]) - (matrix[0][2]*matrix[1][1])
         h = (matrix[0][0]*matrix[1][2]) - (matrix[0][2]*matrix[1][0])
         i = (matrix[0][0]*matrix[1][1]) - (matrix[0][1]*matrix[1][0])
-    # result = [[a, -b, c], [-d, e, -f], [g, -h, i]]
-    adjugated_matrix = [[a, -d, g], [-b, e, -h], [c, -f, i]]
+        # result = [[a, -b, c], [-d, e, -f], [g, -h, i]]
+        adjugated_matrix = [[a, -d, g], [-b, e, -h], [c, -f, i]]
 
-    mat_inverse = mat_inverse = [
-        [entry / determinant(matrix) for entry in row]
-        for row in adjugated_matrix]
-    return mat_inverse
+        mat_inverse = mat_inverse = [
+            [entry / determinant(matrix) for entry in row]
+            for row in adjugated_matrix]
+        return mat_inverse
+    else:
+        # num_rows = len(matrix)
+        determinant_value = determinant(matrix)
+        adjugate_mat = adjugate(matrix)  # Calculate the adjugate matrix
+        inverse_mat = [[entry / determinant_value for entry in row]
+                       for row in adjugate_mat]
+
+        return inverse_mat
