e30d94c3b4f5202c014a7ebc0d6ea6a3762defdc
karanidenis <d.waweru@alustudent.com>
2023-10-19 11:41:16 +0300

    correlation calculation

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -13,27 +13,34 @@ def correlation(C):
     C - numpy.ndarray - covariance matrix, shape (d, d)
     d - int - number of dimensions
     """
-    # C = np.array(C)
-    (d, d) = C.shape
-    if type(C) is not np.ndarray:
+    if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
 
-    # if len(C.shape) != 2:
-    if np.ndim(C) != 2 and d != d:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
     # initialise correlation matrix
-    cor = np.zeros((d, d))
-    print(cor)
-    print(cor.shape)
-    # diagonal elements are 1
-    np.fill_diagonal(cor, 1)
-    print(cor)
-    print(cor.shape)
-    # iterate over rows
-    for i in range(d):
-        # iterate over columns
-        for j in range(d):
-            # calculate correlation
-            cor[i, j] = C[i, j] / np.sqrt(C[i, i] * C[j, j])
+    # cor = np.zeros((d, d))
+    # print(cor)
+    # print(cor.shape)
+    # # diagonal elements are 1
+    # np.fill_diagonal(cor, 1)
+    # print(cor)
+    # print(cor.shape)
+    # # iterate over rows
+    # for i in range(d):
+    #     # iterate over columns
+    #     for j in range(d):
+    #         # calculate correlation
+    #         cor[i, j] = C[i, j] / np.sqrt(C[i, i] * C[j, j])
+    # return cor
+
+    # initialise correlation matrix
+    std_dev = np.sqrt(np.diag(C))
+    print(std_dev)
+    print(std_dev.shape)
+
+    #  calculate correlation matrix
+    cor = C / np.outer(std_dev, std_dev)
+    # print(cor)
     return cor
