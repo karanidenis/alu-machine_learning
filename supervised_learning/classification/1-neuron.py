12849cd2c394e36c6b0e71638864e7d500c4dd81
karanidenis <d.waweru@alustudent.com>
2024-01-15 21:39:31 +0300

    binary classification

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""This module is of a binary classification"""
+import numpy as np
+
+class Neuron:
+    """class that defines a single neuron perfoming binary classification"""
+    
+    def __init__(self, nx):
+        """ class construtor"""
+        
+        # nx - no. of input features to the neuron
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+        
+        # w - weights vector of the neuron
+        self.__W = np.random.normal(0, 1, (nx, 1))
+        
+        # getter function
+        @property
+        def W(self):
+            """getter function"""
+            return self.__W
+        
+        # setter function
+        @W.setter
+        def W(self, value):
+            """setter function"""
+            self.__W = value
+            
+        # Initialize the bias for the neuron
+        self.__b = 0
+        @property
+        def b(self):
+            """getter function"""
+            return self.__b
+        
+        # setter function
+        @b.setter
+        def b(self, value):
+            """setter function"""
+            self.__b = value
+        
+        # Initialize the activated output of the neuron (Prediction)
+        self.__A = 0
+        @property
+        def A(self):
+            """getter function"""
+            return self.__A
+        
+        # setter function
+        @A.setter
+        def A(self, value):
+            """setter function"""
+            self.__A = value
