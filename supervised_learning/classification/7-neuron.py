1c39d8aa6fb5a10e6dac810d1cb22607cc6d02ae
karanidenis <d.waweru@alustudent.com>
2024-01-18 15:43:02 +0300

    binary classification

---
+++ supervised_learning/classification/7-neuron.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+"""This module is of a binary classification"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Neuron:
+    """class that defines a single neuron perfoming binary classification"""
+
+    def __init__(self, nx):
+        """ class construtor"""
+
+        # nx - no. of input features to the neuron
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        # w - weights vector of the neuron
+        self.__W = np.random.normal(0, 1, (nx, 1))
+
+        # Initialize the bias the neuron
+        self.__b = 0
+
+        # Initialize the activated output of the neuron (Prediction)
+        self.__A = 0
+
+    # getter function
+    @property
+    def W(self):
+        """getter function"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter function"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter function"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """calculating forward propagation of the neuron"""
+        # X - a np.ndarray of shape (nx, m)
+        # nx - input features to the neuron
+        # m - no. of examples
+
+        # weighted sum
+        weighted_sum = np.dot(self.__W.T, X) + self.__b
+        # applying activation function
+        self.__A = 1/(1 + np.exp(-weighted_sum))
+
+        return self.__A
+
+    def cost(self, Y, A):
+        """ calculates cost of the model using logistic regression
+        Y - contains correct labels of input data
+        A - contains activated output of the neuron"""
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(Y * np.log(A) +
+                                 (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluating neuron's predictions
+        X - np.ndarray of shape (nx, m) contains input data
+        Y - np.ndarray of shape (1, m) contains correct labels for input data
+        """
+
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        predictions = (A >= 0.5).astype(int)
+        accuracy = np.mean(predictions == Y)
+        return predictions, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """calculates one pass of gradient descent on the neuron
+        X - np.ndarray of shape (nx, m) contains input data
+        Y - np.ndarray of shape (1, m) contains correct labels for input data
+        A - contains activated output of the neuron
+        alpha - learning rate
+        """
+        m = X.shape[1]  # examples
+        dw = np.dot(X, (A - Y).T) / m
+        db = np.sum(A - Y) / m
+
+        # updating weights and bias
+        self.__W = self.__W - alpha*dw
+        self.__b = self.__b - alpha*db
+
+    # def train(self, X, Y, iterations=5000, alpha=0.05):
+    #     """Train the neuron
+    #     X - has input data
+    #     Y - has correct labels for input data
+    #     iterations - no. of iterations to train over
+    #     alpha - learning rate
+    #     """
+    #     if not isinstance(iterations, int):
+    #         raise TypeError("iterations must be an integer")
+    #     if iterations < 1:
+    #         raise ValueError("iterations must be a positive integer")
+
+    #     if not isinstance(alpha, float):
+    #         raise TypeError("alpha must be a float")
+    #     if alpha <= 0.0:
+    #         raise ValueError("alpha must be positive")
+
+    #     for i in range(iterations):
+    #         A = self.forward_prop(X)
+    #         self.gradient_descent(X, Y, A, alpha=0.05)
+    #         # if i % 1000 == 0:
+    #         #     cost = self.cost(Y, A)
+    #         #     print(f"cost after iteration {i}: {cost}")
+
+    #     return self.evaluate(X, Y)
+
+    def train(self, X, Y, iterations=5000, alpha=0.05,
+              verbose=True, graph=True, step=1000):
+        """Training the neuron
+        X - contains input data
+        Y - contains correct lables of input data
+        iterations - no. of iterations to train over
+        """
+
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
+
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0.0:
+            raise ValueError("alpha must be positive")
+
+        if not isinstance(step, int):
+            raise TypeError("step must be an integer")
+
+        if step < 1 or step > iterations:
+            raise ValueError("step must be positive and <= iterations")
+
+        # verbose - boolean to define whether or not to print info about the training
+        # graph - boolean to define whether or not to graph info about training
+        # once training is done
+
+        if graph:
+            # Ensure we include the 0th and last iteration
+            plotting_steps = np.arange(0, iterations + 1, step)
+            if iterations % step != 0:
+                plotting_steps = np.append(plotting_steps, iterations)
+
+            # Initialize plotting_costs
+            plotting_costs = []
+
+            for iteration in range(iterations):
+                A = self.forward_prop(X)
+                self.gradient_descent(X, Y, A, alpha)
+                if (iteration % step) == 0 or iteration == (iterations - 1):
+                    cost = self.cost(Y, A)
+                    plotting_costs.append(cost)
+                    if verbose:
+                        print(f"Cost after {iteration} iterations: {cost}")
+
+            # Plot the costs
+            plt.plot(plotting_steps, plotting_costs)
+            plt.xlabel('iteration')
+            plt.ylabel('cost')
+            plt.title('Training Cost')
+            plt.show()
+
+        return self.evaluate(X, Y)
---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""This module is of a neural network with a hidden layer
+perfomaing binary classification"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """Neuaral network class definition"""
+
+    def __init__(self, nx, nodes):
+        """class constructor
+        nx - no. of input feautures
+        nodes -  number of nodes found in hidden layer"""
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        #  w1 - weight vector of hidden layer
+        self.W1 = np.random.randn(nodes, nx)
+        # b1 - bias of hidden layer
+        self.b1 = np.zeros((nodes, 1))
+        # A1 - activated output of hidden layer
+        self.A1 = 0
+        # w2 - weight vector of output neuron
+        self.W2 = np.random.randn(1, nodes)
+        # b2 - bias of output neuron
+        self.b2 = np.zeros((1 ,nodes))
+        # A2 - activated output of output neuron
+        self.A2 = 0
