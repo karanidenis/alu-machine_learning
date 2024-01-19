16634ee54204264e7b0f14524b638b28daa77091
karanidenis <d.waweru@alustudent.com>
2024-01-19 18:44:46 +0300

    Deep neural networks

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -17,9 +17,15 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if not isinstance(layers, list) or len(layers) < 1:
-            raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        # if not isinstance(layers, list) or len(layers) < 1:
+        #     raise TypeError("layers must be a list of positive integers")
+        # if not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        #     raise TypeError("layers must be a list of positive integers")
+        if (
+            type(layers) is not list
+            or len(layers) < 1
+            or min(layers) < 1
+        ):
             raise TypeError("layers must be a list of positive integers")
 
         # self.L - no. of layers in the neural network
