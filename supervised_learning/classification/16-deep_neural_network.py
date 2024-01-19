9436708d143979f25e607ad18fcd8d7235b79a40
karanidenis <d.waweru@alustudent.com>
2024-01-19 19:31:02 +0300

    Deep neural networks

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -39,6 +39,8 @@ class DeepNeuralNetwork:
 
             else:
                 self.weights['W{}'.format(layer+1)] = np.random.randn(
-                    layers[layer], layers[layer-1]) * np.sqrt(2. / layers[layer-1])
+                    layers[layer],
+                    layers[layer-1]) * np.sqrt(2. / layers[layer-1])
 
-                self.weights['b{}'.format(layer+1)] = np.zeros((layers[layer], 1))
+                self.weights['b{}'.format(
+                    layer+1)] = np.zeros((layers[layer], 1))
