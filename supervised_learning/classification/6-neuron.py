df40e8958baff38f884faf469ff68decf7b9f27a
karanidenis <d.waweru@alustudent.com>
2024-01-20 00:59:23 +0300

    Deep neural networks

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -40,54 +40,81 @@ class Neuron:
         """getter function"""
         return self.__A
 
+    # def forward_prop(self, X):
+    #     """calculating forward propagation of the neuron"""
+    #     # X - a np.ndarray of shape (nx, m)
+    #     # nx - input features to the neuron
+    #     # m - no. of examples
+
+    #     # weighted sum
+    #     weighted_sum = np.dot(self.__W.T, X) + self.__b
+    #     # applying activation function
+    #     self.__A = 1/(1 + np.exp(-weighted_sum))
+
+    #     return self.__A
+
+    # def cost(self, Y, A):
+    #     """ calculates cost of the model using logistic regression
+    #     Y - contains correct labels of input data
+    #     A - contains activated output of the neuron"""
+    #     m = Y.shape[1]
+    #     cost = -(1 / m) * np.sum(Y * np.log(A) +
+    #                              (1 - Y) * np.log(1.0000001 - A))
+    #     return cost
+
+    # def evaluate(self, X, Y):
+    #     """evaluating neuron's predictions
+    #     X - np.ndarray of shape (nx, m) contains input data
+    #     Y - np.ndarray of shape (1, m) contains correct labels for input data
+    #     """
+
+    #     A = self.forward_prop(X)
+    #     cost = self.cost(Y, A)
+    #     predictions = (A >= 0.5).astype(int)
+    #     accuracy = np.mean(predictions == Y)
+    #     return predictions, cost
+
+    # def gradient_descent(self, X, Y, A, alpha=0.05):
+    #     """calculates one pass of gradient descent on the neuron
+    #     X - np.ndarray of shape (nx, m) contains input data
+    #     Y - np.ndarray of shape (1, m) contains correct labels for input data
+    #     A - contains activated output of the neuron
+    #     alpha - learning rate
+    #     """
+    #     m = X.shape[1]  # examples
+    #     dw = np.dot(X, (A - Y).T) / m
+    #     db = np.sum(A - Y) / m
+
+    #     # updating weights and bias
+    #     self.__W = self.__W - alpha*dw
+    #     self.__b = self.__b - alpha*db
     def forward_prop(self, X):
-        """calculating forward propagation of the neuron"""
-        # X - a np.ndarray of shape (nx, m)
-        # nx - input features to the neuron
-        # m - no. of examples
-
-        # weighted sum
-        weighted_sum = np.dot(self.__W.T, X) + self.__b
-        # applying activation function
-        self.__A = 1/(1 + np.exp(-weighted_sum))
-
+        """Calculates the forward propagation of the neuron"""
+        Z = np.dot(self.__W, X) + self.__b
+        sigmoid = 1 / (1 + np.exp(-Z))
+        self.__A = sigmoid
         return self.__A
 
     def cost(self, Y, A):
-        """ calculates cost of the model using logistic regression
-        Y - contains correct labels of input data
-        A - contains activated output of the neuron"""
-        m = Y.shape[1]
-        cost = -(1 / m) * np.sum(Y * np.log(A) +
-                                 (1 - Y) * np.log(1.0000001 - A))
+        """Calculates the cost of the model using logistic regression"""
+        x = 1.0000001 - A
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(x)) / Y.shape[1]
         return cost
 
     def evaluate(self, X, Y):
-        """evaluating neuron's predictions
-        X - np.ndarray of shape (nx, m) contains input data
-        Y - np.ndarray of shape (1, m) contains correct labels for input data
-        """
-
+        """Evaluates the neuron’s predictions"""
         A = self.forward_prop(X)
         cost = self.cost(Y, A)
-        predictions = (A >= 0.5).astype(int)
-        accuracy = np.mean(predictions == Y)
-        return predictions, cost
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
 
     def gradient_descent(self, X, Y, A, alpha=0.05):
-        """calculates one pass of gradient descent on the neuron
-        X - np.ndarray of shape (nx, m) contains input data
-        Y - np.ndarray of shape (1, m) contains correct labels for input data
-        A - contains activated output of the neuron
-        alpha - learning rate
-        """
-        m = X.shape[1]  # examples
-        dw = np.dot(X, (A - Y).T) / m
-        db = np.sum(A - Y) / m
-
-        # updating weights and bias
-        self.__W = self.__W - alpha*dw
-        self.__b = self.__b - alpha*db
+        """Calculates one pass of gradient descent on the neuron"""
+        dz = A - Y
+        dw = np.dot(X, dz.T) / X.shape[1]
+        db = np.sum(dz) / X.shape[1]
+        self._W = self._W - alpha * dw.T
+        self._b = self._b - alpha * db
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """Train the neuron
