72ad55820c3f108e8a83824c910d603324a2d668
karanidenis <d.waweru@alustudent.com>
2024-01-25 12:38:07 +0300

    Tesnsorflow and use case

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -13,3 +13,14 @@ def create_layer(prev, n, activation):
     activation - activation function
     layer - name of layers
     """
+    # He et al. initializer for the layer weights
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
+
+    # Create the layer
+    layer = tf.layers.dense(inputs=prev,
+                            units=n,
+                            activation=activation,
+                            kernel_initializer=initializer,
+                            name='layer')
+    return layer
