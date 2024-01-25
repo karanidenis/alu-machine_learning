8c8a43cfbce9e945cee9b4efa4368370fffb4f9a
karanidenis <d.waweru@alustudent.com>
2024-01-25 12:21:19 +0300

    Tesnsorflow and use case

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -13,7 +13,7 @@ def create_placeholders(nx, classes):
     x - placeholder for input data
     y - placeholder for one-hot labels for input data
     """
-    X = tf.placeholder(tf.float32, hape=[None, nx], name='X')
+    X = tf.placeholder(tf.float32, shape=[None, nx], name='X')
     Y = tf.placeholder(tf.float32, shape=[None, classes], name='Y')
 
     return X, Y
