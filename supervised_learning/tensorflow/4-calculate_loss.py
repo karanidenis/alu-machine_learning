78a84259d3960640ad8cfe30c1b576acdff2b5c2
karanidenis <d.waweru@alustudent.com>
2024-01-26 16:32:08 +0300

    Tesnsorflow and use case

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -14,14 +14,15 @@ def calculate_loss(y, y_pred):
     y_pred - a tensor containing the network's prediction
     returns a tensor containing the loss  of the preiction
     """
-    # Convert y_pred to discrete labels
-    true_labels = tf.argmax(y, 1)
-    predicted_labels = tf.argmax(y_pred, 1)
+    # # Convert y_pred to discrete labels
+    # true_labels = tf.argmax(y, 1)
+    # predicted_labels = tf.argmax(y_pred, 1)
 
     # loss = tf.losses.mean_squared_error(
     # labels=true_labels, predictions=predicted_labels)
     loss = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(
-            labels=true_labels, logits=predicted_labels))
+            labels=y, logits=y_pred))
+    # labels=true_labels, logits=predicted_labels))
 
     return loss
