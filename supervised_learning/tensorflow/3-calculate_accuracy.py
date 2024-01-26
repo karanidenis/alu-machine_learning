5cc94704898abf88f4ccfd0086b835fb070904a4
karanidenis <d.waweru@alustudent.com>
2024-01-26 14:42:55 +0300

    Tesnsorflow and use case

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -14,3 +14,15 @@ def calculate_accuracy(y, y_pred):
     returns a tensor containing the decimal accuracy of
     the prediction
     accuracy = correct_predictions / all_predictions"""
+
+    # Convert y_pred to discrete labels
+    predicted_labels = tf.argmax(y_pred, 1)
+    true_labels = tf.argmax(y, 1)
+
+    # Compare predicted labels with true labels
+    correct_predictions = tf.equal(predicted_labels, true_labels)
+
+    # calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+
+    return accuracy
