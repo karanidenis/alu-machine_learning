cba5066fdb4310bd18c0bb8d6343adf77bf6052d
karanidenis <d.waweru@alustudent.com>
2024-01-31 20:59:06 +0300

    Regularization in machine learning

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -18,7 +18,7 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     # cf = cost + lambda / 2*m * sum(//w//**2)
     l2_penalty = 0
     for layer in range(1, L+1):
-        l2_penalty += (weights[f'W{layer}'] ** 2).sum()
+        l2_penalty += (weights['W{}'.format(layer)] ** 2).sum()
 
     l2_cost = cost + (lambtha / (2*m)) * l2_penalty
 
