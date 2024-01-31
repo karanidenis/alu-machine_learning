e66ee2197c82ae0123f898c63f4d49922db4f534
karanidenis <d.waweru@alustudent.com>
2024-01-31 23:33:02 +0300

    Regularization in machine learning

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""This module has a function that calculates
+cost of  a neural entwork with L2 regularization"""
+
+import numpy as np
+import tensorflow as tf
+
+
+def l2_reg_cost(cost):
+    """calculate cost of the network
+    cost - tensor containing cost of network without L2
+    returns a tensor containing the cost of the network
+    accounting for L2 regularization"""
+
+    # Retrieve regularization losses
+    regularization_losses = tf.add_n(
+        tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+
+    # Add the regularization losses to the original cost
+    total_cost = cost + regularization_losses
+
+    return total_cost
