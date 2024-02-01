f61a880c071b2f8b084b7ec1d3b9f61b44b29953
karanidenis <d.waweru@alustudent.com>
2024-02-01 10:40:29 +0300

    Regularization in machine learning

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -12,12 +12,4 @@ def l2_reg_cost(cost):
     returns a tensor containing the cost of the network
     accounting for L2 regularization"""
 
-    # Retrieve regularization losses
-    # regularization_losses = tf.add_n(
-    # tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
-
-    # Add the regularization losses to the original cost
-    # total_cost = cost + regularization_losses
-    # return total_cost
-
     return cost + tf.losses.get_regularization_losses()
---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""this module has a function that creates
+a tensorflow layer that includes L2 regularization"""
+
+import numpy as np
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    creates a tensorflow layer with L2
+    prev -- tensor with output of previous layer
+    n - no. of nodes new layer should have
+    activation - activatiion function to be used
+    lambtha - L2 regularization param
+    returns output of the new layer
+    """
+    
+    # outputs = activation(inputs * kernel + bias) 
+    # activation - activation function (if not None)
+    # kernel is a weights matrix created by the layer, 
+    # and bias is a bias vector created by the layer (only if use_bias is True).
+    
+    
+    from keras import regularizers
+    from keras.layers import Dense
+    from keras.models import Sequential
+    
+    model = Sequential([
+        Dense(output_dim=n, input_dim=prev, activation=activation,
+        kernel_regularizer=lambtha)])
