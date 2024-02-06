25509ec755ec0dc94a15f952222a6eff4a4fa98b
karanidenis <d.waweru@alustudent.com>
2024-02-06 16:16:13 +0300

    Standardization/normalization

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -16,12 +16,13 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
     X_train - is a numpy.ndarray of shape (m, 784) containing the training data
         m - is the number of data points
         784 - is the number of input features
-    Y_train - is a one-hot numpy.ndarray of shape (m, 10) containing the training labels
+    Y_train - is a one-hot of shape (m, 10) containing the training labels
         10 - is the number of classes the model should classify
-    X_valid - is a numpy.ndarray of shape (m, 784) containing the validation data
-    Y_valid - is a one-hot numpy.ndarray of shape (m, 10) containing the validation labels
+    X_valid - is a of shape (m, 784) with validation data
+    Y_valid - is a one-hot of shape (m, 10) containing the validation labels
     batch_size - is the number of data points in a batch
-    epochs - is the number of times the training should pass through the whole dataset
+    epochs - is the number of times the training should pass
+    through the whole dataset
     load_path - is the path from which to load the model
     save_path - is the path to where the model should be saved after training
     Returns: the path where the model was saved"""
