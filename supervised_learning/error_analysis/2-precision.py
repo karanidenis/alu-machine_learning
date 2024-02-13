77f8bb40c391a39c9a9ae7f8b14e6cf8ba7174dd
karanidenis <d.waweru@alustudent.com>
2024-02-13 19:02:54 +0300

    precision of a confusion matrix

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module conatains a function that
+calculates the precision for
+each class in a confusion matrix"""
+
+import numpy as np
+
+def precision(confusion):
+    """calculte precision
+    confusion - (classes, classes) confusion matrix
+    """
+    classes = confusion.shape[0]
+    precision = np.zeros(classes)
+    for i in range(classes):
+        precision[i] = confusion[i, i] / np.sum(confusion[:, i])
+    return precision
