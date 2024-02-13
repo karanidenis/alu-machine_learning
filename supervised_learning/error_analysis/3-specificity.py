66cbd3197d206955b2e00e380dcadb5f259328cc
karanidenis <d.waweru@alustudent.com>
2024-02-13 19:22:24 +0300

    specifity of a confusion matrix

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -14,9 +14,8 @@ def specificity(confusion):
     classes = confusion.shape[0]  # will be the same as the number of rows
     specificity = np.zeros(classes)  # create an array of zeros
     for i in range(classes):
-        true_negatives = np.sum(
-            confusion) - np.sum(confusion[i]) - np.sum(confusion[:,
-                                                    i]) + confusion[i, i]
+        true_negatives = np.sum(confusion) - np.sum(
+            confusion[i]) - np.sum(confusion[:, i]) + confusion[i, i]
         false_positives = np.sum(confusion[:, i]) - confusion[i, i]
         false_negatives = np.sum(confusion[i]) - confusion[i, i]
         true_positives = confusion[i, i]
