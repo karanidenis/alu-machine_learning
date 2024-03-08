c594df77edd42a08c3fe30f8fc9e56697b68f1f4
karanidenis <d.waweru@alustudent.com>
2024-03-08 12:50:57 +0300

    Swapi API

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -6,9 +6,19 @@ uses Github API to print location of
 specific users"""
 
 import requests
+import sys
 
-# url should come after filename when running the file
-# username should be part of the url
-# response = requests.get(url)
-# data = response.json()
-# print(data['location'])
+# get the url from string passed on the terminal
+
+# first value passed is the url
+url = sys.argv[1]
+
+response = requests.get(url)
+data = response.json()
+if data:
+    if 'location' in data and data['location']:
+        print(data['location'])
+    else:
+        print("Not found")
+else:
+    print("Not found")
