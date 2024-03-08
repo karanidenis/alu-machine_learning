8e602a3e333c9024ab1b55e8bfdcbdfa0f76da2d
karanidenis <d.waweru@alustudent.com>
2024-03-08 12:56:13 +0300

    Github Api

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -10,17 +10,27 @@ import sys
 
 # get the url from string passed on the terminal
 
+# If the user doesn’t exist, print Not found
+# If the status code is 403, print Reset in X min
+# where X is the number of minutes from now and the value of X-Ratelimit-Reset
+# Your code should not be executed when the file is imported
+# (you should use if __name__ == '__main__':)
+
 
 def print_location():
     """print location of user"""
     url = sys.argv[1]
     response = requests.get(url)
     data = response.json()
-    if data:
+
+    if response.status_code == 200:
         if 'location' in data and data['location']:
             print(data['location'])
         else:
             print("Not found")
+    elif response.status_code == 403:
+        print("Reset in {} min".format(
+            (int(response.headers['X-Ratelimit-Reset']) - int(response.headers['X-Ratelimit-Reset'])) / 60))
     else:
         print("Not found")
 
