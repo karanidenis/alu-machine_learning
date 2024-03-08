1803d02984e224fe16bf7c77c3bb8e41b46fcc90
karanidenis <d.waweru@alustudent.com>
2024-03-08 12:52:58 +0300

    Github Api

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -10,15 +10,20 @@ import sys
 
 # get the url from string passed on the terminal
 
-# first value passed is the url
-url = sys.argv[1]
 
-response = requests.get(url)
-data = response.json()
-if data:
-    if 'location' in data and data['location']:
-        print(data['location'])
+def print_location():
+    """print location of user"""
+    url = sys.argv[1]
+    response = requests.get(url)
+    data = response.json()
+    if data:
+        if 'location' in data and data['location']:
+            print(data['location'])
+        else:
+            print("Not found")
     else:
         print("Not found")
-else:
-    print("Not found")
+
+
+if __name__ == "__main__":
+    print_location()
