2d17ea74af2459d6b0651fbe58f37cb73f48f09f
karanidenis <d.waweru@alustudent.com>
2024-03-08 12:34:32 +0300

    Swapi API

---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function that
+uses Swapi API to get the list of names of
+all sentinent species in the Star Wars universe"""
+
+import requests
+
+def sentientPlanets():
+    """return list of names of all sentient species"""
+    url = "https://swapi-api.alx-tools.com/api/species/"
+    response = requests.get(url)
+    data = response.json()
+    species = []
+    for result in data['results']:
+        if result['designation'] == "sentient":
+            species.append(result['name'])
+    return species
