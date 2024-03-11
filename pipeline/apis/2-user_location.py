c38555f8fbda4aa6b98fb97b675d209209a1a092
karanidenis <d.waweru@alustudent.com>
2024-03-11 12:54:35 +0300

    Github api

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -7,6 +7,7 @@ specific users"""
 
 import requests
 import sys
+import time
 
 # get the url from string passed on the terminal
 
@@ -23,17 +24,16 @@ def print_location():
     response = requests.get(url)
     data = response.json()
 
-    if response.status_code == 200:
-        if 'location' in data and data['location']:
-            print(data['location'])
-        else:
-            print("Not found")
-    elif response.status_code == 403:
-        print("Reset in {} min".format(
-            (int(response.headers['X-Ratelimit-Reset']) -
-             int(response.headers['X-Ratelimit-Reset'])) / 60))
-    else:
+    if response.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+
+    elif response.status_code == 404:
         print("Not found")
+    elif response.status_code == 200:
+        print(data['location'])
 
 
 if __name__ == "__main__":
