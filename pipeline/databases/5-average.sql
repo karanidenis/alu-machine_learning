1322d45a580775517b9b86f2fe308d3e6883ecb8
karanidenis <d.waweru@alustudent.com>
2024-03-12 10:23:01 +0300

    calculate average of score values

--- pipeline/databases/5-average.sql
+++ pipeline/databases/5-average.sql
@@ -1,3 +1,5 @@
 -- compute the average of the values in the second_table
+-- The result column name should be average
 
-SELECT AVG(score) FROM second_table;
+-- SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
