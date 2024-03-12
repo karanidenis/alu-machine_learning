60e69f4ec432eb4a633c5a43aa3818cbe00f9609
karanidenis <d.waweru@alustudent.com>
2024-03-12 10:33:21 +0300

    sort cities and temperatures in a db

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- display average tempeqature(Fahrenheit) for each city ordered by temp in descending order
 
-SELECT city, AVG(temperature) AS avg_temp FROM temperatures GROUP BY city ORDER BY average_temperature DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
