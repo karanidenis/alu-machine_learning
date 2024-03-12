def3ae9dc6a647c6390e960830d1d443f4f8e03c
karanidenis <d.waweru@alustudent.com>
2024-03-12 10:14:56 +0300

    create 2nd table and list with a condition

--- pipeline/databases/4-best_score.sql
+++ pipeline/databases/4-best_score.sql
@@ -1,3 +1,5 @@
 -- list all records with a score >= 10 in 2nd table second_table
+-- display only score and name in this order and top to bottom
 
-SELECT * FROM second_table WHERE score >= 10;
+-- SELECT * FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10;
