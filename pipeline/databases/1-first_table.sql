ca4469dd5080cffb90bf70a2d7b6d83e7f73884c
karanidenis <d.waweru@alustudent.com>
2024-03-12 09:59:00 +0300

    first table

---
+++ pipeline/databases/1-first_table.sql
@@ -0,0 +1,11 @@
+-- create first table in db_0, 
+-- first_table:
+-- id: int
+-- name: varchar(256)
+
+-- db name will be passed as an argument of mysql command
+
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT,
+  name VARCHAR(256)
+);
