58434c571a0e063198ed40b0ea09778716b6490c
karanidenis <d.waweru@alustudent.com>
2024-03-13 00:04:09 +0300

    count shows per genre

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -6,5 +6,9 @@
 -- sort by number of shows linked to genre in descending order
 -- use only one SELECT statement
 
-SELECT tv_show_genres.genre_id AS genre, 
-COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, 
+       COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
