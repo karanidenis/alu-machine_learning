eacff6bcc44b7b4a53bad366ae84fa74de6eb360
karanidenis <d.waweru@alustudent.com>
2024-03-14 11:23:23 +0300

    triggers

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,14 @@
+-- create a trigger that decreases quantity of an item after adding a new order
+-- quantity can be negative
+
+DROP TRIGGER IF EXISTS decrease_quantity;
+DELIMITER //
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - 1
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER ;
