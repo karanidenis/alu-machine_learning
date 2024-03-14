51cc099c9ea60f935692cd03486de12f0ede57be
karanidenis <d.waweru@alustudent.com>
2024-03-14 15:58:59 +0300

    logs

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -16,9 +16,11 @@ print(f"{total_logs} logs")
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 for method in methods:
     count = collection.count_documents({"method": method})
-    print(f"    method {method}: {count}")
+    # print(f"    method {method}: {count}")
+    print("method {}: {}".format(method, count))
 
 # Count number of documents with method=GET and path=/status
 status_check_count = collection.count_documents(
     {"method": "GET", "path": "/status"})
-print(f"{status_check_count} status check")
+# print(f"{status_check_count} status check")
+print("status check: {}".format(status_check_count))
