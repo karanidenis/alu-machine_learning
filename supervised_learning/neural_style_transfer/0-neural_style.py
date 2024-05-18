261594cfabba1042262d4f3104905b569d176108
karanidenis <d.waweru@alustudent.com>
2024-05-18 14:39:41 +0300

    rescaling images to 512 pixels

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -8,10 +8,10 @@ import numpy as np
 import tensorflow as tf
 
 
-
 class NST:
     """class NST"""
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
@@ -21,25 +21,30 @@ class NST:
         alpha - weight for content cost
         beta - weight for style cost
         """
-        self.style_image = style_image
-        self.content_image = content_image
-        
+        # tensorflow executes eagerly
+        tf.enable_eager_execution()
+
+        error = "style_image must be a numpy.ndarray with shape (h, w, 3)"
         if not isinstance(style_image, np.ndarray):
-            raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
+            raise TypeError(error)
         if style_image.ndim != 3 or style_image.shape[2] != 3:
-            raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
-            
+            raise TypeError(error)
+
+        error = "content_image must be a numpy.ndarray with shape (h, w, 3)"
         if not isinstance(content_image, np.ndarray):
-            raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
+            raise TypeError(error)
         if content_image.ndim != 3 or content_image.shape[2] != 3:
-            raise TypeError("style_image must be a numpy.ndarray with shape (h, w, 3)")
+            raise TypeError(error)
                 
-        if alpha >= 0:
+        if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
         
-        if beta < 0:
+        if not isinstance(beta, (int, float)) or beta < 0:
             raise TypeError("beta must be a non-negative number")
+
         
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
         
