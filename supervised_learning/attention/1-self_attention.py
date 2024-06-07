ab47504578cd4d336727ab4deda9c5f8d4802651
karanidenis <d.waweru@alustudent.com>
2024-06-07 20:45:51 +0300

    self attention

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -36,4 +36,5 @@ class SelfAttention(tf.keras.layers.Layer):
         V = self.V(tf.nn.tanh(W + U))
         weights = tf.nn.softmax(V, axis=1)
         context = tf.reduce_sum(weights * hidden_states, axis=1)
+
         return context, weights
