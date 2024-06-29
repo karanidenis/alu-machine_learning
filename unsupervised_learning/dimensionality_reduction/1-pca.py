9c9298beb1d55e1d5cd85c2a1555da25c4a527eb
karanidenis <d.waweru@alustudent.com>
2024-06-29 21:19:41 +0300

    dimensionality reduction

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -20,12 +20,18 @@ def pca(X, ndim):
     T: numpy.ndarray: (n, ndim) the transformed
     version of X
     """
-    X = X - np.mean(X, axis=0)
-    cov = np.cov(X, rowvar=False)
-    eigvals, eigvecs = np.linalg.eig(cov)
-    idx = np.argsort(eigvals)[::-1]
-    eigvecs = eigvecs[:, idx]
-    W = eigvecs[:, :ndim]
-    T = np.matmul(X, W)
+    # X = X - np.mean(X, axis=0)
+    # cov = np.cov(X, rowvar=False)
+    # eigvals, eigvecs = np.linalg.eig(cov)
+    # idx = np.argsort(eigvals)[::-1]
+    # eigvecs = eigvecs[:, idx]
+    # W = eigvecs[:, :ndim]
+    # T = np.matmul(X, W)
 
-    return T
+    # return T
+    mean = np.mean(X, axis=0, keepdims=True)
+    A = X - mean
+    u, s, v = np.linalg.svd(A)
+    W = v.T[:, :ndim]
+    T = np.matmul(A, W)
+    return (T)
