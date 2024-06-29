ff66c44c9f4a9c8c723d58dec3d9f7441630ce83
karanidenis <d.waweru@alustudent.com>
2024-06-29 22:04:15 +0300

    clustering

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -6,6 +6,7 @@ expectation step in the EM algorithm for a GMM
 """
 
 import numpy as np
+pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
@@ -24,25 +25,16 @@ def expectation(X, pi, m, S):
             probabilities for each data point in each cluster
         -l: log likelihood of the model
     """
-    pdf = __import__('5-pdf').pdf
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None
-    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
-        return None, None
-    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
-        return None, None
-    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+    if len(X.shape) != 2 or len(S.shape) != 3\
+            or len(pi.shape) != 1 or len(m.shape) != 2\
+            or m.shape[1] != X.shape[1]\
+            or S.shape[2] != S.shape[1]\
+            or S.shape[0] != pi.shape[0]\
+            or S.shape[0] != m.shape[0]\
+            or np.min(pi) < 0:
         return None, None
     n, d = X.shape
     k = pi.shape[0]
-    if d != m.shape[1] or d != S.shape[1] or d != S.shape[2]:
-        return None, None
-    if k != m.shape[0] or k != S.shape[0]:
-        return None, None
-    if not np.isclose(np.sum(pi), 1):
-        return None, None
-    if not all(np.isclose(np.sum(pi), 1) for pi in S):
-        return None, None
     g = np.zeros((k, n))
     for i in range(k):
         g[i] = pi[i] * pdf(X, m[i], S[i])
