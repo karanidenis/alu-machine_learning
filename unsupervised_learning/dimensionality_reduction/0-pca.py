af7759281ffcee7e1d7aba322d364be08e55b5a7
karanidenis <d.waweru@alustudent.com>
2024-06-29 21:17:45 +0300

    dimensionality reduction

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -24,29 +24,35 @@ def pca(X, var=0.95):
     """
 
     # Center the data
-    X = X - np.mean(X, axis=0)
+    # X = X - np.mean(X, axis=0)
 
-    # Compute the covariance matrix
-    cov = np.cov(X, rowvar=False)
+    # # Compute the covariance matrix
+    # cov = np.cov(X, rowvar=False)
 
-    # Compute the eigenvalues and eigenvectors
-    eigvals, eigvecs = np.linalg.eig(cov)
+    # # Compute the eigenvalues and eigenvectors
+    # eigvals, eigvecs = np.linalg.eig(cov)
 
-    # Sort the eigenvectors by decreasing eigenvalues
-    idx = np.argsort(eigvals)[::-1]
-    eigvecs = eigvecs[:, idx]
+    # # Sort the eigenvectors by decreasing eigenvalues
+    # idx = np.argsort(eigvals)[::-1]
+    # eigvecs = eigvecs[:, idx]
 
-    # Compute the total variance
-    total_var = np.sum(eigvals)
+    # # Compute the total variance
+    # total_var = np.sum(eigvals)
 
-    # Compute the number of components to keep
-    k = 0
-    for i in range(len(eigvals)):
-        if np.sum(eigvals[:i+1]) / total_var >= var:
-            k = i + 1
-            break
+    # # Compute the number of components to keep
+    # k = 0
+    # for i in range(len(eigvals)):
+    #     if np.sum(eigvals[:i+1]) / total_var >= var:
+    #         k = i + 1
+    #         break
 
-    # Select the k eigenvectors
-    W = eigvecs[:, :k]
+    # # Select the k eigenvectors
+    # W = eigvecs[:, :k]
 
-    return W
+    # return W
+    u, s, v = np.linalg.svd(X)
+    ratios = list(x / np.sum(s) for x in s)
+    variance = np.cumsum(ratios)
+    nd = np.argwhere(variance >= var)[0, 0]
+    W = v.T[:, :(nd + 1)]
+    return (W)
