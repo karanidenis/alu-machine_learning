bd6089e35211f110450c78047c85b25a483f7b69
karanidenis <d.waweru@alustudent.com>
2024-07-07 22:48:45 +0300

    markov chain

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -21,19 +21,39 @@ def regular(P):
     steady state probabilities, or None on failure
     """
 
-    if type(P) is not np.ndarray or len(P.shape) != 2:
-        return None
-    n, n = P.shape
-    if n != P.shape[0]:
-        return None
-    if np.sum(P, axis=1).all() != 1:
-        return None
-    if np.any(P <= 0):
-        return None
+    # if type(P) is not np.ndarray or len(P.shape) != 2:
+    #     return None
+    # n, n = P.shape
+    # if n != P.shape[0]:
+    #     return None
+    # if np.sum(P, axis=1).all() != 1:
+    #     return None
+    # if np.any(P <= 0):
+    #     return None
 
-    evals, evecs = np.linalg.eig(P.T)
-    evecs = evecs.T
-    for i in range(len(evals)):
-        if np.allclose(evals[i], 1):
-            return evecs[i] / np.sum(evecs[i])
-    return None
+    # evals, evecs = np.linalg.eig(P.T)
+    # evecs = evecs.T
+    # for i in range(len(evals)):
+    #     if np.allclose(evals[i], 1):
+    #         return evecs[i] / np.sum(evecs[i])
+    # return None
+    try:
+        if len(P.shape) != 2:
+            return None
+        n = P.shape[0]
+        if n != P.shape[1]:
+            return None
+
+        #  (πP).T = π.T ⟹ P.T π.T = π.T (.)
+        evals, evecs = np.linalg.eig(P.T)
+
+        # trick: has to be normalized
+        state = (evecs / evecs.sum())
+
+        # P.T π.T = π.T (.)
+        new_state = np.dot(state.T, P)
+        for i in new_state:
+            if (i >= 0).all() and np.isclose(i.sum(), 1):
+                return i.reshape(1, n)
+    except Exception:
+        return None
